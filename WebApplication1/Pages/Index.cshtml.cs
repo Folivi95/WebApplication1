e38361e2ca9b91bffd69312cd8d7e39149eab101
Folivi Joshua Oluwaseun <joshua.folivi@gmail.com>
2018-09-22 23:04:38 +0100

    Created Edit and Index Page

--- WebApplication1/Pages/Index.cshtml.cs
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -1,17 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication1.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace WebApplication1.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly AppDbContext _db;
 
+        public IndexModel(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public List<Customer> Customers { get; private set; }
+
+        public async Task OnGetAsync()
+        {
+            Customers = await _db.Customers.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync(int id)
+        {
+            var contact = await _db.Customers.FindAsync(id);
+
+            if (contact != null)
+            {
+                _db.Customers.Remove(contact);
+                await _db.SaveChangesAsync();
+            }
+
+            return RedirectToPage();
         }
     }
 }
